a2e23a8cb92ad998d84746a3af2562e9647d956f
LanLi2017 <lilan.scut@gmail.com>
2018-09-19 22:53:10 -0500

    linear and parallel

--- openrefine-client-master/runtimeLinear.py
+++ openrefine-client-master/runtimeLinear.py
@@ -1,20 +1,49 @@
+import json
 import time
 
 from google.refine import refine
 
+file_path=raw_input('input the file path: ')
+file_name=raw_input('input the file name: ')
+
+
+def judgeFunction(dicts,projectID):
+    if dicts['op']=='core/column-rename':
+            oldcol=dicts['oldColumnName']
+            newcol=dicts['newColumnName']
+            refine.RefineProject(refine.RefineServer(),projectID).rename_column(oldcol,newcol)
+    elif dicts['op']=='core/text-transform':
+        columnName=dicts['columnName']
+        expression=dicts['expression']
+        refine.RefineProject(refine.RefineServer(),projectID).text_transform(columnName,expression)
+    elif dicts['op']=='core/column-split':
+        columnName=dicts['columnName']
+        separator=dicts['separator']
+        refine.RefineProject(refine.RefineServer(),projectID).split_column(columnName,separator)
+
+
 start_time=time.time()
+
 # input raw dataset A
-projectID=refine.Refine(refine.RefineServer()).new_project('Menupart.csv','Portal_rodents Linear')[1]
 
-# operation B: Lowercase column sponsor
-refine.RefineProject(refine.RefineServer(),projectID).text_transform('sponsor','value.toLowercase()')
+projectID=refine.Refine(refine.RefineServer()).new_project(file_path,file_name)[1]
 
-# operation C: Numeric column id
-refine.RefineProject(refine.RefineServer(),projectID).text_transform('id','value.toNumber()')
+# operation B: Lowercase column sponsor operation C;....
+# Linear operations :
+with open('runtime_Model.json')as f:
+    data=json.load(f)
+
+for dicts in data:
+    judgeFunction(dicts,projectID)
+
+# refine.RefineProject(refine.RefineServer(),projectID).text_transform('sponsor','value.toLowercase()')
+#
+# # operation C: Numeric column id
+# refine.RefineProject(refine.RefineServer(),projectID).text_transform('id','value.toNumber()')
 
 end_time=time.time()
 
 total_time=end_time-start_time
 print('Linear time: '+ str(total_time))
 
-# 0.0769319534302
+# Linear time: 0.243105888367
--- openrefine-client-master/runtimeSP.py
+++ openrefine-client-master/runtimeSP.py
@@ -1,26 +1,44 @@
+import json
 import time
 from multiprocessing import Pool, Process
 
 from google.refine import refine
 
+file_path=raw_input('input the file path: ')
+file_name=raw_input('input the file name: ')
+
+
 start_time=time.time()
 # input raw dataset A
-projectID=refine.Refine(refine.RefineServer()).new_project('Menupart.csv','Portal_rodentsSP4')[1]
+projectID=refine.Refine(refine.RefineServer()).new_project(file_path,file_name)[1]
 
-#parallel operation B and operation C
-def do_work(work_type):
-    if work_type==1:
-        refine.RefineProject(refine.RefineServer(),projectID).text_transform('sponsor','value.toLowercase()')
-    elif work_type==2:
-        refine.RefineProject(refine.RefineServer(),projectID).text_transform('id','value.toNumber()')
+with open('runtime_Model.json')as f:
+    data=json.load(f)
+
+# parallel operation B and operation C
+def do_work(dicts):
+    if dicts['op']=='core/column-rename':
+            oldcol=dicts['oldColumnName']
+            newcol=dicts['newColumnName']
+            refine.RefineProject(refine.RefineServer(),projectID).rename_column(oldcol,newcol)
+    elif dicts['op']=='core/text-transform':
+        columnName=dicts['columnName']
+        expression=dicts['expression']
+        refine.RefineProject(refine.RefineServer(),projectID).text_transform(columnName,expression)
+    elif dicts['op']=='core/column-split':
+        columnName=dicts['columnName']
+        separator=dicts['separator']
+        refine.RefineProject(refine.RefineServer(),projectID).split_column(columnName,separator)
 
 
 
 if __name__=='__main__':
+
     p=Pool(2)
-    p.map(do_work,[1,2])
+    p.map(do_work,[dicts for dicts in data])
     end_time=time.time()
     total_time=end_time-start_time
     print('sp time: '+str(total_time))
 
-# sp time: 0.0615239143372
+
+# sp time: 0.0958931446075
