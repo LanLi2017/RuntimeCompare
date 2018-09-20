b434c07f5669fac3bf36e1b99b42afb03b43b8f2
LanLi2017 <lilan.scut@gmail.com>
2018-09-20 00:34:31 -0500

    parallel update

--- openrefine-client-master/runtime_compare.py
+++ openrefine-client-master/runtime_compare.py
@@ -14,7 +14,8 @@ projectID=refine.Refine(refine.RefineServer()).new_project(file_path,file_name)[
 with open('runtime_Model.json')as f:
     data=json.load(f)
 
-# need further develop...
+
+# need further develop for the ...
 def OR_operation(dicts):
     if dicts['op']=='core/column-rename':
             oldcol=dicts['oldColumnName']
@@ -29,34 +30,35 @@ def OR_operation(dicts):
         separator=dicts['separator']
         refine.RefineProject(refine.RefineServer(),projectID).split_column(columnName,separator)
 
-# loop 1000 times for linear model
-def Linear_time_it(func, times=1000):
+
+# pool for  parallel processes
+p=Pool(5)
+
+
+# loop 1000 times for two models
+def time_it(func, times=1000):
     start = time.time()
     for _ in range(times):
         func()
     end = time.time()
-    print('linear time: '+str(end-start))
+    return end-start
 
 
 # Linear task
-def Linear_data():
+def Linear_model():
     for dicts in data:
         OR_operation(dicts)
 
 
-# loop 1000 times for parallel model
-def Parallel_time_it():
-    p=Pool(5)
-    start = time.time()
-    for _ in range(1000):
-        p.map(OR_operation,data)
-    end = time.time()
-    print('Parallel time: '+str(end - start))
+# Parallel task
+def Parallel_model():
+    p.map(OR_operation,data)
 
 
 def main():
-    Parallel_time_it()
-    Linear_time_it(Linear_data)
+    print('Parallel time: '+str(time_it(Parallel_model))+'\n')
+    print('Linear time: '+str(time_it(Linear_model))+'\n')
+
 
 
 if __name__=='__main__':
