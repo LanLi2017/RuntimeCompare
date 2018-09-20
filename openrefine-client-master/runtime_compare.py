4c40858ff57fca22762d93a62f88ac110a6a5507
LanLi2017 <lilan.scut@gmail.com>
2018-09-20 00:39:14 -0500

    reformat the python script

--- openrefine-client-master/runtime_compare.py
+++ openrefine-client-master/runtime_compare.py
@@ -1,38 +1,37 @@
 import json
 import time
-from multiprocessing import Pool, Process
+from multiprocessing import Pool
 
 from google.refine import refine
 
-file_path='Menupart.csv'
-file_name='MenupartSPtest'
-
+file_path = 'Menupart.csv'
+file_name = 'MenupartSPtest'
 
 # input raw dataset A
-projectID=refine.Refine(refine.RefineServer()).new_project(file_path,file_name)[1]
+projectID = refine.Refine(refine.RefineServer()).new_project(file_path, file_name)[1]
 
 with open('runtime_Model.json')as f:
-    data=json.load(f)
+    data = json.load(f)
 
 
-# need further develop for the ...
-def OR_operation(dicts):
-    if dicts['op']=='core/column-rename':
-            oldcol=dicts['oldColumnName']
-            newcol=dicts['newColumnName']
-            refine.RefineProject(refine.RefineServer(),projectID).rename_column(oldcol,newcol)
-    elif dicts['op']=='core/text-transform':
-        columnName=dicts['columnName']
-        expression=dicts['expression']
-        refine.RefineProject(refine.RefineServer(),projectID).text_transform(columnName,expression)
-    elif dicts['op']=='core/column-split':
-        columnName=dicts['columnName']
-        separator=dicts['separator']
-        refine.RefineProject(refine.RefineServer(),projectID).split_column(columnName,separator)
+# need further develop for the OR operation...
+def or_operation(dicts):
+    if dicts['op'] == 'core/column-rename':
+        oldcol = dicts['oldColumnName']
+        newcol = dicts['newColumnName']
+        refine.RefineProject(refine.RefineServer(), projectID).rename_column(oldcol, newcol)
+    elif dicts['op'] == 'core/text-transform':
+        columnname = dicts['columnName']
+        expression = dicts['expression']
+        refine.RefineProject(refine.RefineServer(), projectID).text_transform(columnname, expression)
+    elif dicts['op'] == 'core/column-split':
+        columnname = dicts['columnName']
+        separator = dicts['separator']
+        refine.RefineProject(refine.RefineServer(), projectID).split_column(columnname, separator)
 
 
 # pool for  parallel processes
-p=Pool(5)
+p = Pool(5)
 
 
 # loop 1000 times for two models
@@ -41,27 +40,24 @@ def time_it(func, times=1000):
     for _ in range(times):
         func()
     end = time.time()
-    return end-start
+    return end - start
 
 
 # Linear task
-def Linear_model():
+def linear_model():
     for dicts in data:
-        OR_operation(dicts)
+        or_operation(dicts)
 
 
 # Parallel task
-def Parallel_model():
-    p.map(OR_operation,data)
+def parallel_model():
+    p.map(or_operation, data)
 
 
 def main():
-    print('Parallel time: '+str(time_it(Parallel_model))+'\n')
-    print('Linear time: '+str(time_it(Linear_model))+'\n')
+    print('Parallel time: ' + str(time_it(parallel_model)) + '\n')
+    print('Linear time: ' + str(time_it(linear_model)) + '\n')
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
